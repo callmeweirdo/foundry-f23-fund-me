3e16a348210c68e66f19d724cf49496e4a4beff4
callmeweirdo <ogbandav102@gmail.com>
2024-01-08 18:51:20 +0100

    ive done the network configuration to switch between the types of network to use during the deploy proccess, which can either be a live network (Sepolia testnet) or Anvil local server network

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -4,11 +4,18 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMde is Script{
     function run() external returns (FundMe) {
+
+        HelperConfig helperConfig = new HelperConfig();
+
+        address ethPriceUsd = helperConfig.getNetworkConfig().priceFeed; 
+
+
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethPriceUsd);
         vm.stopBroadcast();
         return fundMe;
     }
