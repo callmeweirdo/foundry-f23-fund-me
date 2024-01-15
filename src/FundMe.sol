ccdd5eb7703354e8c0fa84fda0226cea5cc5a34a
callmeweirdo <ogbandav102@gmail.com>
2024-01-15 23:36:09 +0100

    commented

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import {PriceConverter} from "./PriceConverter.sol";
+// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+// import {PriceConverter} from "./PriceConverter.sol";
 
 import "forge-std/console.sol";
 
@@ -93,5 +93,3 @@ contract FundMe {
 // 5. abi.encode / decode
 // 6. Hash with keccak256
 // 7. Yul / Assembly
-
-
