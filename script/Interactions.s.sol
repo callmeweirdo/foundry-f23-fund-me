ece80c45ec1b4501941c02ad4342c837ac96057a
callmeweirdo <ogbandav102@gmail.com>
2024-01-23 00:40:36 +0100

    just installed a foundry-devops library from github

---
+++ script/Interactions.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+
+contract FundFundMe is Script{
+    uint256 constant SEND_VALUE = 0.01 ether;
+        
+}
