e661402526ae16a70d06bf07d995b6a90696220b
callmeweirdo <ogbandav102@gmail.com>
2024-01-25 05:48:28 +0100

    setting up the interaction test contract

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -10,14 +10,27 @@ contract FundFundMe is Script{
     uint256 constant SEND_VALUE = 0.01 ether;
     
     function fundFundMe(address mostRecentlyDeployed) public {
-        vm.startBroadcast();
         FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
-        vm.stopBroadcast();
         console.log("funded the FundMe with the value of", SEND_VALUE);
     }
 
     function run() public {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        vm.startBroadcast();
         fundFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
+    }
+}
+
+contract WithdrawFundMe is Script{
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+    }
+
+    function run() public {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        vm.startBroadcast();
+        withdrawFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
     }
 }
--- test/InteractionsTest.t.sol
+++ test/InteractionsTest.t.sol
@@ -5,8 +5,31 @@ pragma solidity ^0.8.18;
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMde} from "../script/DeployFundMe.s.sol";
+import {FundFundMe} from "../script/Interactions.s.sol";
 
 
-contract IteractionTest{
-    
+contract IteractionTest is Test{
+    FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_AMOUNT = 10 ether;
+
+    function setUp() public {
+        DeployFundMde deployFundMe = new DeployFundMde();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_AMOUNT);
+    }
+
+    function testUserCanFundInteraction() public{
+        FundFundMe fundFundMe = new FundFundMe();
+
+        vm.prank(USER);
+        vm.deal(USER, 1e18);
+        fundFundMe.fundFundMe(address(fundMe));
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+
 }
